97583eedd9948e98203a743dcc2911e38f5230e5
eozant <eozant@st.swps.edu.pl>
2025-11-04 20:27:56 +0100

    Update Clean name.py

--- Clean name.py
+++ Clean name.py
@@ -1,12 +1,5 @@
-name = input("Enter your name: ")
+name = ' Ege '
+print(name.strip())
+print(name.lstrip())
+print(name.rstrip())
 
-print(f"As entered: '{name}'")
-
-stripped_name = name.strip()
-print(f"After strip(): '{stripped_name}'")
-
-lstripped_name = name.lstrip()
-print(f"After lstrip(): '{lstripped_name}'")
-
-rstripped_name = name.rstrip()
-print(f"After rstrip(): '{rstripped_name}'")
